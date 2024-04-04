54a235c25729b99498adbb6458abdf60ab4b11d9
DungNT <tridungtin8@gmail.com>
2024-04-04 15:21:12 +0700

    create backend folder

---
+++ backend/leetcode-clone/src/test/java/com/college/leetcodeclone/LeetcodeCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.leetcodeclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
