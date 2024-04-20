2feecc5370753c8ae5c17837150788eaa27d7b0c
DungNT <tridungtin8@gmail.com>
2024-04-20 17:01:44 +0700

    add data cleaner schedule

--- backend/leetcode-clone/src/main/java/com/college/leetcodeclone/LeetcodeCloneApplication.java
+++ backend/leetcode-clone/src/main/java/com/college/leetcodeclone/LeetcodeCloneApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @PropertySource({"classpath:server.properties"})
+@EnableScheduling
 public class LeetcodeCloneApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeetcodeCloneApplication.class, args);
--- backend/leetcode-clone/src/main/java/com/college/leetcodeclone/repository/AccountRepository.java
+++ backend/leetcode-clone/src/main/java/com/college/leetcodeclone/repository/AccountRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,6 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    List<Account> findByIsEnable(boolean b);
 }
